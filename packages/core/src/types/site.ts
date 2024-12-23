059ee10b2b810fbdc063a8fa1b551f6587a71713
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2024-12-23 14:39:57 +0100

    feat: implement configurable types

--- packages/core/src/types/site.ts
+++ packages/core/src/types/site.ts
@@ -1,43 +1,44 @@
 import type { Field } from "./field";
 import type {
-	allowedMethodsForChildren,
-	allowedMethodsForFiles,
-	allowedMethodsForModels,
+  allowedMethodsForChildren,
+  allowedMethodsForFiles,
+  allowedMethodsForModels,
 } from "./allowedMethods";
 import type { Page, Pages } from "./page";
 
 export interface Site
-	extends allowedMethodsForChildren,
-		allowedMethodsForFiles,
-		allowedMethodsForModels {
-	__extra: Field;
-	__default: {
-		title: string;
-		children: string[];
-		url: string;
-	};
-	// blueprint: () => Blueprint;
-	breadcrumb: () => Pages;
-	errorPage: () => Page;
-	errorPageId: () => string;
-	exists: () => boolean;
-	homePage: () => Page;
-	homePageId: () => string;
-	mediaUrl: () => string;
-	modified: (format: string, handler: string) => string | number;
-	page: (path: string) => Page;
-	pages: () => Pages;
-	panel: () => Site;
-	children: () => Pages;
-	// permissions: () => SitePermissions;
-	previewUrl: () => string;
-	// search: (query: string, params: Array | string) => Pages;
-	// url: (language: string) => string; // fields: () => Array;
-	// isDefault: () => boolean;
-	// name: () => string;
-	// section: (name: string) => Section;
-	// sections: () => Array;
-	// tab: (name: string) => Array;
-	// tabs: () => Array;
-	title: () => Field;
+  extends KQL.Site,
+    allowedMethodsForChildren,
+    allowedMethodsForFiles,
+    allowedMethodsForModels {
+  __extra: Field;
+  __default: {
+    title: string;
+    children: string[];
+    url: string;
+  };
+  // blueprint: () => Blueprint;
+  breadcrumb: () => Pages;
+  errorPage: () => Page;
+  errorPageId: () => string;
+  exists: () => boolean;
+  homePage: () => Page;
+  homePageId: () => string;
+  mediaUrl: () => string;
+  modified: (format: string, handler: string) => string | number;
+  page: (path: string) => Page;
+  pages: () => Pages;
+  panel: () => Site;
+  children: () => Pages;
+  // permissions: () => SitePermissions;
+  previewUrl: () => string;
+  // search: (query: string, params: Array | string) => Pages;
+  // url: (language: string) => string; // fields: () => Array;
+  // isDefault: () => boolean;
+  // name: () => string;
+  // section: (name: string) => Section;
+  // sections: () => Array;
+  // tab: (name: string) => Array;
+  // tabs: () => Array;
+  title: () => Field;
 }
---
+++ packages/core/types.d.ts
@@ -0,0 +1,10 @@
+declare namespace KQL {
+  interface Page {}
+  interface Site {}
+  interface App {}
+  interface File {}
+}
+
+declare namespace Blocks {
+  interface KirbyDefaultBlocks {}
+}
