3552f5323948f698b80d44e32d316d5ca026d0bd
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2025-01-19 17:54:33 +0100

    fix: formatting and add docs for Blocks namespace

--- packages/core/types.d.ts
+++ packages/core/types.d.ts
@@ -1,33 +1,33 @@
 declare namespace KQL {
-	/**
-	 * Represents a Kirby page. you can add custom fields and methods to it. using this interface.
-	 */
-	interface Page {}
-	/**
-	 * Represents the Kirby site object. you can add custom fields and methods to it. using this interface.
-	 */
-	interface Site {}
-	/**
-	 * Represents the Kirby file object. you can add custom fields and methods to it. using this interface
-	 */
-	interface File {}
-	/**
-	 * Represents the Kirby object. you can add custom fields and methods to it. using this interface
-	 */
-	interface App {}
+  /**
+   * Represents a Kirby page. you can add custom fields and methods to it. using this interface.
+   */
+  interface Page {}
+  /**
+   * Represents the Kirby site object. you can add custom fields and methods to it. using this interface.
+   */
+  interface Site {}
+  /**
+   * Represents the Kirby file object. you can add custom fields and methods to it. using this interface
+   */
+  interface File {}
+  /**
+   * Represents the Kirby object. you can add custom fields and methods to it. using this interface
+   */
+  interface App {}
 }
 
 declare namespace Blocks {
-	/**
-	 * Stores all the default blocks that are available in Kirby.
-	 * You can add custom blocks to this interface.
-	 * @example
-	 * declare global {
-	 *  namespace Blocks {
-	 *   interface KirbyDefaultBlocks {
-	 *    customBlock: {
-	 *     customField: string;
-	 *   };
-	 */
-	interface KirbyDefaultBlocks {}
+  /**
+   * Stores all the default blocks that are available in Kirby.
+   * You can add custom blocks to this interface.
+   * @example
+   * declare global {
+   *  namespace Blocks {
+   *   interface KirbyDefaultBlocks {
+   *    customBlock: {
+   *     customField: string;
+   *   };
+   */
+  interface KirbyDefaultBlocks {}
 }
