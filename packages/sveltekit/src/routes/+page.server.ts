ecd1e33d75e0676e7cda796a19a5535aac4b8997
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2025-01-27 13:08:30 +0100

    refactor: remove unused imports and simplify load function

--- packages/sveltekit/src/routes/+page.server.ts
+++ packages/sveltekit/src/routes/+page.server.ts
@@ -1,8 +1,6 @@
 import type { PageServerLoad } from './$types.js';
-import type { KQLQueryTypeResolver } from '@kql-ts/core';
-import { page } from '@kql-ts/core';
+
 export const load: PageServerLoad = async () => {
-	let test = 'test';
 	return {
 		test: 'hello world'
 	};
