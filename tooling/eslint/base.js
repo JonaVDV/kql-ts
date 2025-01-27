649e820adaa1f1758d4b5fb6e5b1fce771c3e0fa
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2025-01-27 13:05:23 +0100

    feat: add proper linting

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -1,25 +1,27 @@
-import {includeIgnoreFile} from '@eslint/compat';
-import js from '@eslint/js';
-import ts from 'typescript-eslint';
-import globals from 'globals';
-import {fileURLToPath} from 'bun'
-import prettier from 'eslint-config-prettier';
+/// <reference types="./types.d.ts" />
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+import { includeIgnoreFile } from "@eslint/compat";
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import globals from "globals";
+import { fileURLToPath } from "url";
+import prettier from "eslint-config-prettier";
+
+const gitignorePath = fileURLToPath(new URL("../../.gitignore", import.meta.url));
 
 export default ts.config(
-  includeIgnoreFile(gitignorePath),
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  prettier,
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	prettier,
 
-  {
-    ignores: ['**/*.config.*', '**/*.d.ts'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    }
-  }
+	{
+		ignores: ["**/*.config.*", "**/*.d.ts"],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+	}
 );
